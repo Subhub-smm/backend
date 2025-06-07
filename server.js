e0c51df96d51d27a45c26f5ec2176a791e9b5304
Otim Emmanuel Oyuku <subhubug@gmail.com>
2025-06-07 15:43:18 +0300

    ✅ Fixed DB connection with object config

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.use(bodyParser.json());
 
 console.log("🚀 Connecting to DB...");
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
 
 db.connect((err) => {
   if (err) {
